d5941088d8623a3fe352b5d9b0249132791313ec
khezamikhalil <99364595+khezamikhalil@users.noreply.github.com>
2022-02-13 00:05:53 +0100

    Add files via upload

---
+++ sc.js
@@ -0,0 +1,23 @@
+var i=0;
+function calc()
+{
+    i++;
+    document.formclick.nb_click.value = (i);
+
+}
+
+ /* var i;
+function calc()
+{
+    i--;
+    document.formclick.nb_click.value = (i);
+}  */
+
+function calcr()
+{
+    if(i!=0)
+    {
+        i--;
+        document.formclick.nb_click.value=(i);
+    }
+}
